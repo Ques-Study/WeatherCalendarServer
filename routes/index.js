17b6b6fc74230afa0dd2aa1d0dbde686055936ef
Chris Jeong <crowjdh@gmail.com>
2017-07-24 13:26:28 +0000

    Implement basic structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Index = require('../controllers/Index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Index.run(req, res, next);
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
