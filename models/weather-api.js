d0dfe2db5198faad6544c6e08240ca197785928a
나동현 <donghyun@nadonghyeon-ui-MacBook-Pro.local>
2017-07-31 16:28:44 +0900

    pull request first feedback

--- models/weather-api.js
+++ models/weather-api.js
@@ -2,9 +2,7 @@ var rest = require('restler');
 var parser = require('xml2json');
 
 module.exports.fetch = function() {
-    
     return new Promise(function(resolve, reject) {
-        var result = {};
         rest.get('http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=2723067100')
         .on('complete', function(data) {
             if(data instanceof Error){
@@ -15,3 +13,4 @@ module.exports.fetch = function() {
         });
     });
 }
+ 
--- tests/weather-fetcher.spec.js
+++ tests/weather-fetcher.spec.js
@@ -3,11 +3,11 @@ const weatherFetcher = require('../models/weather-fetcher.js');
 
 describe("Weather fetcher", function() {
   it('promise return value should exist', function() {
-    return weatherFetcher.fetch().then(function(data){
-        var items = data["rss"]["channel"]["item"]["description"]["body"]["data"];
-        
-        should.exist(data);
-        items.should.have.length(15);
+    return weatherFetcher.fetch().then(function(jsonData){
+      should.exist(jsonData);
+      var localWeather = jsonData["rss"]["channel"]["item"]["description"]["body"]["data"];      
+      localWeather.should.have.length(15);
     });
   });
 });
+ 
