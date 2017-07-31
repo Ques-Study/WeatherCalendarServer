cf6de5fbb69eb20484224498a374a69ec1d06bd9
Seongjeong Choi <refsta21@gmail.com>
2017-07-31 09:36:57 +0000

    Refactor test
    - Merge two tests into one file.
    - Clean up database after test.
    - Fix wrong testing while saving/retrieving object into mongodb.

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -1,9 +1,26 @@
 const should = require('should');
 const mongoose = require('mongoose');
+const dailyWeathers = require("../models/weatherSchema");
 
-describe("MongoDB", function (){
-  it("should be able to connect to db", function (done) {   
-    mongoose.connect('mongodb://localhost:27017/worktest', function (err, db) {
+describe("MongoDB", function() {
+  afterEach(function() {
+    if (this.connection != 'undefined') {
+      dailyWeathers.collection.drop();
+    }
+  });
+
+  after(function() {
+    if (this.connection != 'undefined') {
+      this.connection.connection.db.dropDatabase(function() {
+        console.log("dropped.");
+      });
+      this.connection.connection.close();
+    }
+  });
+
+  it("should be able to connect to db", function(done) {
+    // TODO: Refactor dependency
+    this.connection = mongoose.connect('mongodb://localhost:27017/worktest', function(err, db) {
       if (err) {
         should.exist(null);
         done();
@@ -13,17 +30,19 @@ describe("MongoDB", function (){
       }
     });
   });
-  it("should be able to create object", function (done) {
-    const Cat = mongoose.model('Cat', { name: String });
-    const kitty = new Cat({ name: 'Zildjian' });
-    kitty.save(function (err) {
-      if (err) {
-        should.exist(null);
-        done();
-      } else {
-        should.exist(true);
-        done();
-      }
+
+  it("should save and load weather", function (done) {
+    const weather = new dailyWeathers({
+      minTem : "21",
+      maxTem : "35",
+      windSpeed : "4",
+      humidity : "37",
+      weather : "sunny"
     });
-  })
+    weather.save(function (err, out) {
+      should.not.exist(err);
+      should.equal(out, weather);
+      done();
+    });
+  });
 });
