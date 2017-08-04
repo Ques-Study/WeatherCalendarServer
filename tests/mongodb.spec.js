00a7fe82396710185c9c309db3d1c011426a9c23
Frownii <refsta21@gmail.com>
2017-08-04 16:27:36 +0900

    Update mongodb.spec.js
    3, 8, 21 Change the name of model and database to simple name including capital letter
    11 Remove unnecessary function
    15, 16 Refactor if-else statement to better one

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -1,38 +1,24 @@
 const should = require('should');
 const mongoose = require('mongoose');
-const dailyWeathers = require("../models/weatherSchema");
+const Weather = require("../models/weatherSchema");
 
 describe("MongoDB", function() {
   afterEach(function() {
     if (this.connection != 'undefined') {
-      dailyWeathers.collection.drop();
-    }
-  });
-
-  after(function() {
-    if (this.connection != 'undefined') {
-      this.connection.connection.db.dropDatabase(function() {
-        console.log("dropped.");
-      });
-      this.connection.connection.close();
+      Weather.collection.drop();
     }
   });
 
   it("should be able to connect to db", function(done) {
     // TODO: Refactor dependency
     this.connection = mongoose.connect('mongodb://localhost:27017/worktest', function(err, db) {
-      if (err) {
-        should.exist(null);
-        done();
-      } else {
-        should.exist(true);
-        done();
-      }
+      should.not.exist(err);
+      done();
     });
   });
 
   it("should save and load weather", function (done) {
-    const weather = new dailyWeathers({
+    const weather = new Weather({
       minTem : "21",
       maxTem : "35",
       windSpeed : "4",
