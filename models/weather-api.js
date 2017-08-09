66b061961a8e2e08ea43d506c1797965a6e50e10
나동현 <donghyun@nadonghyeon-ui-MacBook-Pro.local>
2017-08-10 00:23:05 +0900

    - add filtering function and test

--- models/weather-api.js
+++ models/weather-api.js
@@ -1,5 +1,6 @@
 var rest = require('restler');
 var parser = require('xml2json');
+const FILTERING_KEY_ARRAY = ["hour", "day", "temp", "sky", "pty", "pop"];
 
 module.exports.fetch = function() {
     return new Promise(function(resolve, reject) {
@@ -8,8 +9,21 @@ module.exports.fetch = function() {
             if(data instanceof Error){
                 reject(error);
             } else {
-                resolve(JSON.parse(parser.toJson(data)));
+                var apiResponse = JSON.parse(parser.toJson(data));
+                resolve(filterJSON(FILTERING_KEY_ARRAY, apiResponse["rss"]["channel"]["item"]["description"]["body"]["data"]));
             }
         });
     });
+}
+
+function filterJSON(FILTERING_KEY_ARRAY, responseArray){
+    var filteredArray = [];
+    responseArray.forEach(function(responseElement) {
+        var filteredUnitObject = {};
+        FILTERING_KEY_ARRAY.forEach(function(key) {
+            filteredUnitObject[key] = responseElement[key];
+        }, this);
+        filteredArray.push(filteredUnitObject);
+    }, this);    
+    return filteredArray;
 }
--- tests/weather-api.spec.js
+++ tests/weather-api.spec.js
@@ -1,14 +1,30 @@
 const should = require('should');
 const weatherApi = require('../models/weather-api.js');
 const FORECAST_COUNT_PER_DAY = 8;
+const KEYS_OF_OBJECT_IN_WEATHER_ARRAY = ["hour", "day", "temp", "sky", "pty", "pop"];
+
 
 describe("Weather fetcher", function() {
-  it('promise return value should exist', function() {
-    return weatherApi.fetch().then(function(apiResponse){
-      should.exist(apiResponse);
-      // TODO: weather JSON에서 추출하는 로직을 따로 뺄 것.
-      var weather = apiResponse["rss"]["channel"]["item"]["description"]["body"]["data"];      
-      weather.length.should.be.aboveOrEqual(FORECAST_COUNT_PER_DAY);
-    });
+  var weatherArray = [];
+
+  before(function() {
+    return weatherApi.fetch().then(function(data) {
+      weatherArray = data;
+    })
+  });
+
+  it('promise return value should exist', function(done) {
+    should.exist(weatherArray);
+    weatherArray.length.should.be.aboveOrEqual(FORECAST_COUNT_PER_DAY);
+    done();
+  });
+
+  it('Every element in Array has remanded keys and values', function(done) {
+    weatherArray.forEach(function(weatherElement) {
+      KEYS_OF_OBJECT_IN_WEATHER_ARRAY.forEach(function(key) {
+        weatherElement.should.have.properties(key);  
+      }, this);
+    }, this);
+    done();
   });
 });
