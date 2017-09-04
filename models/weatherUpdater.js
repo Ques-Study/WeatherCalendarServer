e5cb78330a9f2f31a719f4a1f8183cf2cebb0918
Frownii <refsta21@gmail.com>
2017-09-04 13:05:23 +0000

    Modify updating logic in weather update model

--- models/weatherUpdater.js
+++ models/weatherUpdater.js
@@ -1,16 +1,16 @@
 const Weather = require("./weatherSchema");
 const weatherApi = require("./weather-api");
 
-module.exports.updateWeatherData = function(weather) {
+module.exports.updateWeather = function(weathers) {
   return new Promise(function(resolve, reject) {
     var savedCount = 0;
-    for (var i = 0; i < weather.length; i++) {
-      updateWeather(weather[i], function(err) {
+    for (var i = 0; i < weathers.length; i++) {
+      updateWeather(weathers[i], function(err) {
         if (err) {
           reject(err);
           return;
         }
-        if (weather.length == ++savedCount) {
+        if (weathers.length == ++savedCount) {
           resolve();
         }
       });
@@ -22,47 +22,60 @@ function updateWeather(dailyWeather, callback) {
   Weather.findOne({ date: dailyWeather.date }, function(err, oldWeather){
     if (err) {
       console.log("db err");
+			callback(err);
+			return;
     }
     if(!oldWeather){
-      saveNewDailyWeathers(dailyWeather);
-			callback(err);
+      saveNewDailyWeathers(dailyWeather, callback);
     } else {
-      updateHourlyWeather(oldWeather, dailyWeather);
-			callback(err);
+      updateHourlyWeather(oldWeather, dailyWeather, callback);
     }
   });
 }
 
-function saveNewDailyWeathers(newDailyWeather) {
+function saveNewDailyWeathers(newDailyWeather, callback) {
 	newDailyWeather.save(function(err) {
 		if(err) {
 		  console.log("Save err");
+  		callback(err);
 		} else {
       console.log("Weather saved");
+			callback();
 		}
 	});
 }
 
-function updateHourlyWeather(oldWeather, newWeather) {
+function updateHourlyWeather(oldWeather, newWeather, callback) {
 	const date = newWeather.date
-	const updatedWeathers = setUpdatedWeatherArray(oldWeather, newWeather);
+	const updatedWeathers = getUpdatedHourlyWeathers(oldWeather, newWeather);
 	Weather.update({ date: date }, { $set: { weathers: updatedWeathers }}, function(err) {
 		if(err) {
 			console.log("Update err");
+  		callback(err);
 		} else {
 			console.log("Weather updated");
+			callback();
 		}
 	});
 }
 
-function setUpdatedWeatherArray(oldWeatherObject, newWeatherObject) {
-	const oldWeathersArrayLength = oldWeatherObject.weathers.length - newWeatherObject.weathers.length;
-	const updatedWeathers = [];
-	for(var i = 0; i<oldWeathersArrayLength; i++) {
-		updatedWeathers.push(oldWeather.weathers[i]);
+function getUpdatedHourlyWeathers(oldWeather, newWeather) {
+	if (newWeather.weathers.length == 0) {
+		return oldWeather.weathers;
 	}
-	newWeatherObject.weathers.forEach(function(newWeather) {
-		updatedWeathers.push(newWeather);
+	
+	const firstHourOfNewWeather = newWeather.weathers[0].hour;
+	var updatedHourlyWeathers = [];
+	var oldHourlyWeathers = oldWeather.weathers.filter(function(weather) {
+		return weather.hour < firstHourOfNewWeather;
 	});
-	return updatedWeathers;
+	oldHourlyWeathers.forEach(function(oldHourlyWeather) {
+		updatedHourlyWeathers.push(oldHourlyWeather);
+	});
+	newWeather["weathers"].forEach(function(newHourlyWeather) {
+		updatedHourlyWeathers.push(newHourlyWeather);
+	});
+
+	return updatedHourlyWeathers;
 }
+
