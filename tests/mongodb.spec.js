c1ad8c5fdaae4d5581f3f72d67444b96b9fb4ec2
Frownii <refsta21@gmail.com>
2017-09-04 23:08:40 +0900

    Update mongodb.spec.js

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -3,43 +3,43 @@ const mongoose = require('mongoose');
 const Weather = require("../models/weatherSchema");
 
 describe("MongoDB", function() {
-  before(function(done) {
-    mongoose.Promise = global.Promise;
-    this.connection = mongoose.connect('mongodb://localhost:27017/worktest', function(err, db) {
-      should.not.exist(err);
-      done();
-    });
-  });
+	before(function(done) {
+		mongoose.Promise = global.Promise;
+		this.connection = mongoose.connect('mongodb://localhost:27017/worktest', function(err, db) {
+			should.not.exist(err);
+			done();
+		});
+	});
 
-  after(function(done) {
-    mongoose.disconnect(function() {
-      done();
-    });
-  });
+	after(function(done) {
+		mongoose.disconnect(function() {
+			done();
+		});
+	});
 
-  afterEach(function() {
-    if (this.connection != 'undefined') {
-      Weather.collection.drop();
-    }
-  });
+	afterEach(function() {
+		if (this.connection != 'undefined') {
+			Weather.collection.drop();
+		}
+	});
 
-  it("should save and load weather", function (done) {
-    const weather = new Weather({
-      date: new Date(),
-      weathers: [{
-        hour: 6,
-        temp: 27,
-        skyCode: 0,
-        rainfallCode: 1,
-        rainfallProbability : 60,
-      }]
-    });
-    weather.save(function (err, out) {
-      should.not.exist(err);
-      should.equal(out, weather);
-      done();
-    });
-  });
+	it("should save and load weather", function (done) {
+		const weather = new Weather({
+			date: new Date(),
+			weathers: [{
+				hour: 6,
+				temp: 27,
+				skyCode: 0,
+				rainfallCode: 1,
+				rainfallProbability : 60,
+			}]
+		});
+		weather.save(function (err, out) {
+			should.not.exist(err);
+			should.equal(out, weather);
+			done();
+		});
+	});
 
 });
 
