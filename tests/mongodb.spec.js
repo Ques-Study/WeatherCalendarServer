82fff85fbeb3612000f3247176ba724560e7c92e
Frownii <refsta21@gmail.com>
2017-09-04 11:19:21 +0000

    Add db disconnection logic in db test

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -3,19 +3,24 @@ const mongoose = require('mongoose');
 const Weather = require("../models/weatherSchema");
 
 describe("MongoDB", function() {
-  afterEach(function() {
-    if (this.connection != 'undefined') {
-      Weather.collection.drop();
-    }
-  });
-
-  it("should be able to connect to db", function(done) {
-    // TODO: Refactor dependency
+	before(function(done) {
     mongoose.Promise = global.Promise;
     this.connection = mongoose.connect('mongodb://localhost:27017/worktest', function(err, db) {
       should.not.exist(err);
       done();
     });
+	});
+
+	after(function(done) {
+		mongoose.disconnect(function() {
+			done();
+		});
+	});
+
+  afterEach(function() {
+    if (this.connection != 'undefined') {
+      Weather.collection.drop();
+    }
   });
 
   it("should save and load weather", function (done) {
